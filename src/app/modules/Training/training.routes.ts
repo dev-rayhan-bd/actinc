bc52958cfe7a41dfce2b1f825467f7022c04f161
rayhan108 <raihanshorker56@gmail.com>
2026-07-27 22:04:30 +0600

    give permission

--- src/app/modules/Training/training.routes.ts
+++ src/app/modules/Training/training.routes.ts
@@ -25,7 +25,7 @@ router.post(
 
 router.get(
   '/',
-  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin,USER_ROLE.company,USER_ROLE.user,USER_ROLE.guest),
   TrainingControllers.getAllTrainings,
 );
 
